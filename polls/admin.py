e93e2c73167ffdedf65972a8de9698ca5d385aae
Federico Mendez <mendezfederico87@gmail.com>
2013-02-01 20:51:14 +0100

    added basic admin for polls app

---
+++ polls/admin.py
@@ -0,0 +1,19 @@
+from polls.models import Poll, Choice
+from django.contrib import admin
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class PollAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
+    inlines = (ChoiceInline, )
+
+admin.site.register(Poll, PollAdmin)
+
+
+
