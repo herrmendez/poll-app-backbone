bae0b892c9482a523b236feaa8a63a0cc88bbb9a
Federico Mendez <mendezfederico87@gmail.com>
2013-02-02 21:05:51 +0100

    added index view

--- polls/views.py
+++ polls/views.py
@@ -1,1 +1,8 @@
-# Create your views here.
+from django.shortcuts import render_to_response
+from django.http import HttpResponse
+
+
+def index(request):
+    return render_to_response('polls/index.html', {} )
+
+
