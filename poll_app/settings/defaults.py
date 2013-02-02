14fd9473c815cbadfc31927efd6e941786c90881
Federico Mendez <mendezfederico87@gmail.com>
2013-02-02 18:29:23 +0100

    added djangorestframework

--- poll_app/settings/defaults.py
+++ poll_app/settings/defaults.py
@@ -121,6 +121,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.admin',
+    'rest_framework',
     'polls',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
--- poll_app/urls.py
+++ poll_app/urls.py
@@ -5,13 +5,6 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'poll_app.views.home', name='home'),
-    # url(r'^poll_app/', include('poll_app.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^', include('polls.urls')),
 )
