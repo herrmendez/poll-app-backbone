0073b17b03c1af1dbf049569f5f642b682a9aa38
Federico Mendez <mendezfederico87@gmail.com>
2013-02-07 20:01:29 +0100

    added script to serialize forms to json

--- poll_app/static/js/base.js
+++ poll_app/static/js/base.js
@@ -1,3 +1,24 @@
 $(function() {
   $('#pub_date').datepicker({ dateFormat: "dd/mm/yy"});
 });
+
+$.fn.serializeObject = function()
+{
+    /*
+     * jQuery doesn't have the ability to serialize objects so we'll use
+     * this little script to do that (useful to convert form data to json).
+     */ 
+    var o = {};
+    var a = this.serializeArray();
+    $.each(a, function() {
+        if (o[this.name] !== undefined) {
+            if (!o[this.name].push) {
+                o[this.name] = [o[this.name]];
+            }
+            o[this.name].push(this.value || '');
+        } else {
+            o[this.name] = this.value || '';
+        }
+    });
+    return o;
+};
